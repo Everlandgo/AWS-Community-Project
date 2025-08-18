02bd22ced26609778f0a24ee3d29823388b0c8c0
Jungeon Song <songjungeon@gmail.com>
2025-08-18 11:41:49 +0900

    editing the routers

--- app/db.py
+++ app/db.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # SQLite 로컬 DB
-DATABASE_URL = "sqlite:///./local.db"
+DATABASE_URL = "sqlite:///C:/Users/SD1-13/Desktop/AWS Community/local.db"
 
+# SQLAlchemy 엔진 생성
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -8,8 +8,12 @@ app = FastAPI(title="FastAPI Comment Service")
 models.Base.metadata.create_all(bind=db.engine)
 
 # 라우터 등록
-app.include_router(router, prefix="/api")
-
+app.include_router(router, prefix="/api", tags=["Comments"])
+app = FastAPI(
+    title="FastAPI Comment Service",
+    openapi_url="/api/openapi.json",  # OpenAPI 정의 파일 경로
+    docs_url="/api/docs"              # Swagger UI 경로
+)
 @app.get("/health", tags=["Health"])
 def health_check():
     return {"status": "ok"}
